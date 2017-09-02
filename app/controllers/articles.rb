38feb3e4ae492776e3905ffd5614dfbda624c4d2
Oscar Diaz <o-pipe@hotmail.com>
2017-09-02 18:40:53 -0500

    Crea la funcionalidad de crear articulo

--- app/controllers/articles.rb
+++ app/controllers/articles.rb
@@ -7,3 +7,11 @@ end
 get "/new/article" do
 	erb :'articles/new'	
 end
+
+post "/articles" do
+	article = HTTParty.post('http://localhost:3000/api/v1/articles', body: { image_url: params['image_url']  ,code: params['code'], description: params['description']}.to_json, headers: {"Authorization" => "Token token=\"GNwqAsap2omwigQyuLQQnToG\"", 'Content-Type' => 'application/json' })
+	redirect "/"
+end
+
+
+ 
