04e0bb73f34a4cc463d9bf0db9ec23da2ae2ab57
Oscar Diaz <o-pipe@hotmail.com>
2017-09-02 21:09:37 -0500

    Crea la funcionalidad de Editar el Movimiento

--- app/controllers/movements.rb
+++ app/controllers/movements.rb
@@ -10,11 +10,16 @@ end
 
 post "/movements" do
 	movements = HTTParty.post('http://localhost:3000/api/v1/movements', body: { movement_type_id: params['movement_type_id'], article_id: params['article_id'], amount: params['amount']}.to_json, headers: {"Authorization" => "Token token=\"GNwqAsap2omwigQyuLQQnToG\"", 'Content-Type' => 'application/json' })
-	redirect "/movements"
+	redirect "/"
 end
 
 get "/edit/movement/:id" do
 	@articles = HTTParty.get("http://localhost:3000/api/v1/articles", headers: {'Content-Type' => 'application/json',"Authorization" => "Token token=\"GNwqAsap2omwigQyuLQQnToG\""})
 	@movement = HTTParty.get("http://localhost:3000/api/v1/movements/#{params[:id]}", headers: {"Authorization" => "Token token=\"GNwqAsap2omwigQyuLQQnToG\""})
 	erb :'movements/edit'
+end
+
+patch "/movement/:id" do
+	@movement = HTTParty.patch("http://localhost:3000/api/v1/movements/#{params[:id]}", body: { movement_type_id: params['movement_type_id'], article_id: params['article_id'], amount: params['amount']}.to_json, headers: {"Authorization" => "Token token=\"GNwqAsap2omwigQyuLQQnToG\"", 'Content-Type' => 'application/json' })
+	redirect "/"
 end
