4f5b47a70444b4c7b1ee99a36229fbd959dd1ccf
Oscar Diaz <o-pipe@hotmail.com>
2017-09-02 19:23:48 -0500

    Crea la funcionalidad de Editar Articulo

--- app/controllers/articles.rb
+++ app/controllers/articles.rb
@@ -1,6 +1,5 @@
 get "/" do
 	@articles = HTTParty.get("http://localhost:3000/api/v1/articles", headers: {'Content-Type' => 'application/json',"Authorization" => "Token token=\"GNwqAsap2omwigQyuLQQnToG\""})
-	@total = HTTParty.get("http://localhost:3000/api/v1/total_articles/2", headers: {"Authorization" => "Token token=\"GNwqAsap2omwigQyuLQQnToG\""})
 	erb :'articles/index'
 end
 
@@ -10,6 +9,16 @@ end
 
 post "/articles" do
 	article = HTTParty.post('http://localhost:3000/api/v1/articles', body: { image_url: params['image_url']  ,code: params['code'], description: params['description']}.to_json, headers: {"Authorization" => "Token token=\"GNwqAsap2omwigQyuLQQnToG\"", 'Content-Type' => 'application/json' })
+	redirect "/"
+end
+
+get "/edit/article/:id" do
+	@article = HTTParty.get("http://localhost:3000/api/v1/articles/#{params[:id]}", headers: {"Authorization" => "Token token=\"GNwqAsap2omwigQyuLQQnToG\""})
+	erb :'articles/edit'
+end
+
+patch "/article/:id" do
+	@article = HTTParty.patch("http://localhost:3000/api/v1/articles/#{params[:id]}", body: { image_url: params['image_url']  ,code: params['code'], description: params['description']}.to_json, headers: {"Authorization" => "Token token=\"GNwqAsap2omwigQyuLQQnToG\"", 'Content-Type' => 'application/json' })
 	redirect "/"
 end
 
